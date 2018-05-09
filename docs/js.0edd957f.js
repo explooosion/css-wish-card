85fbc7e74af93f13633e3ec5588ef4765130265a
Robby <ta7382@gmail.com>
2018-05-09 19:35:04 +0800

    Update public url

--- docs/js.0edd957f.js
+++ docs/js.0edd957f.js
@@ -1,6 +1,6 @@
-parcelRequire=function(e,r,n){var t="function"==typeof parcelRequire&&parcelRequire,i="function"==typeof require&&require;function u(n,o){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!o&&f)return f(n,!0);if(t)return t(n,!0);if(i&&"string"==typeof n)return i(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}a.resolve=function(r){return e[n][1][r]||r};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,a,l,l.exports)}return r[n].exports;function a(e){return u(a.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=t;for(var o=0;o<n.length;o++)u(n[o]);return u}({6:[function(require,module,exports) {
+parcelRequire=function(e,r,n){var t="function"==typeof parcelRequire&&parcelRequire,i="function"==typeof require&&require;function u(n,o){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!o&&f)return f(n,!0);if(t)return t(n,!0);if(i&&"string"==typeof n)return i(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}a.resolve=function(r){return e[n][1][r]||r};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,a,l,l.exports)}return r[n].exports;function a(e){return u(a.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=t;for(var o=0;o<n.length;o++)u(n[o]);return u}({8:[function(require,module,exports) {
 
-},{}],2:[function(require,module,exports) {
+},{}],4:[function(require,module,exports) {
 "use strict";require("papercss/dist/paper.css"),require("../scss/index.scss");
-},{"papercss/dist/paper.css":6,"../scss/index.scss":6}]},{},[2])
-//# sourceMappingURL=/js.daff1a73.map
+},{"papercss/dist/paper.css":8,"../scss/index.scss":8}]},{},[4])
+//# sourceMappingURL=js.daff1a73.map
